562132708806c1b5b03b00b635a5086fb44cb011
SalvatoreBia <salvatorebia02@gmail.com>
2023-12-27 17:39:49 +0100

    push del controller per registrarsi + servlet logout

---
+++ backend/src/main/java/it/brasilenacode/chesshub/controller/RegisterController.java
@@ -0,0 +1,45 @@
+package it.brasilenacode.chesshub.controller;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import it.brasilenacode.chesshub.persistenza.DAO.UtenteDao;
+import it.brasilenacode.chesshub.persistenza.DAO.postgres.UtenteDaoPostgres;
+import it.brasilenacode.chesshub.persistenza.DBManager;
+import it.brasilenacode.chesshub.persistenza.model.Utente;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+public class RegisterController {
+
+    @PostMapping("/register")
+    public boolean registerUser(@RequestBody String payload) {
+
+        UtenteDao userDao = DBManager.getInstance().getUtenteDao();
+        ObjectMapper objectMapper = new ObjectMapper();
+        Utente inputUser = null;
+        try {
+            inputUser = objectMapper.readValue(payload, new TypeReference<Utente>(){});
+        } catch (IOException e) {
+            System.out.println("ERROR TRYING TO PARSE JSON TO OBJECT Utente.");
+            e.printStackTrace();
+            return false;
+        }
+
+        if (inputUser != null) {
+            Utente tmpUser = userDao.findByPrimaryKey(inputUser.getUsername());
+            if (tmpUser == null) {
+                userDao.saveOrUpdate(inputUser);
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
