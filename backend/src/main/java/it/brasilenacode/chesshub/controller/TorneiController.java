2b049bfc4a8d104d70bfa1d7e89aa2163eeabbdb
Davide Pirrò <david.pirro02@gmail.com>
2023-12-28 07:21:46 +0100

    modificati TorneiController e UtenteController nel backend

--- backend/src/main/java/it/brasilenacode/chesshub/controller/TorneiController.java
+++ backend/src/main/java/it/brasilenacode/chesshub/controller/TorneiController.java
@@ -1,30 +1,51 @@
 package it.brasilenacode.chesshub.controller;
 
+import it.brasilenacode.chesshub.persistenza.DBManager;
 import it.brasilenacode.chesshub.persistenza.model.Torneo;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import it.brasilenacode.chesshub.persistenza.model.Utente;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @RestController
 @CrossOrigin(value = "http://localhost:4200/", allowCredentials = "true")
 public class TorneiController {
-
     @PostMapping("/tornei/stato")
-    public List<Torneo> dammiTorneiStato() {
-        Torneo t = new Torneo();
-        List<Torneo> tornei = new ArrayList<>();
-        t.setNome("Torneo 1");
-        t.setLuogo("Roma");
-        t.setStato("In corso");
-        t.setDataInizio(new java.util.Date());
-        t.setDataFine(new java.util.Date());
-        t.setNumeroPartecipanti(10);
-        t.setId(1);
-        tornei.add(t);
-        return tornei;
+    public List<Torneo> dammiTorneiStato(@RequestBody String body) {
+        return DBManager.getInstance().getTorneoDao().findAll().stream().filter(torneo -> torneo.getStato().equals(body)).toList();
+    }
+    @PostMapping("/tornei/numeroPartecipantiMaggiore")
+    public List<Torneo> dammiTorneiConPiuPartecipanti(@RequestBody String body) {
+        return DBManager.getInstance().getTorneoDao().findAll().stream().filter(torneo -> torneo.getNumeroPartecipanti() > Integer.parseInt(body)).toList();
+    }
+    @PostMapping("/tornei/numeroPartecipantiMinore")
+    public List<Torneo> dammiTorneiConMenoPartecipanti(@RequestBody String body) {
+        return DBManager.getInstance().getTorneoDao().findAll().stream().filter(torneo -> torneo.getNumeroPartecipanti() < Integer.parseInt(body)).toList();
+    }
+    @PostMapping("/tornei/numeroPartecipantiUguale")
+    public List<Torneo> dammiTorneiConEsattamentePartecipanti(@RequestBody String body) {
+        return DBManager.getInstance().getTorneoDao().findAll().stream().filter(torneo -> torneo.getNumeroPartecipanti() == Integer.parseInt(body)).toList();
+    }
+    @PostMapping("/tornei/nome")
+    public List<Torneo> dammiTorneiNome(@RequestBody String body) {
+        return DBManager.getInstance().getTorneoDao().findAll().stream().filter(torneo -> torneo.getNome().equals(body)).toList();
+    }
+    @PostMapping("/tornei/luogo")
+    public List<Torneo> dammiTorneiLuogo(@RequestBody String body) {
+        return DBManager.getInstance().getTorneoDao().findAll().stream().filter(torneo -> torneo.getLuogo().equals(body)).toList();
+    }
+    @PostMapping("/tornei/dataInizio")
+    public List<Torneo> dammiTorneiDataInizio(@RequestBody String body) {
+        return DBManager.getInstance().getTorneoDao().findAll().stream().filter(torneo -> torneo.getDataInizio().toInstant().equals(new Date(body).toInstant())).toList();
+    }
+    @PostMapping("/tornei/dataFine")
+    public List<Torneo> dammiTorneiDataFine(@RequestBody String body) {
+        return DBManager.getInstance().getTorneoDao().findAll().stream().filter(torneo -> torneo.getDataInizio().toInstant().equals(new Date(body).toInstant())).toList();
+    }
+    @PostMapping("/tornei/vincitore")
+    public List<Torneo> dammiTorneiVincitore(@RequestBody String body) {
+        return DBManager.getInstance().getTorneoDao().findAll().stream().filter(torneo -> torneo.getVincitore().equals(DBManager.getInstance().getUtenteDao().findByPrimaryKey(body))).toList();
     }
 }
--- backend/src/main/java/it/brasilenacode/chesshub/controller/UtentiController.java
+++ backend/src/main/java/it/brasilenacode/chesshub/controller/UtentiController.java
@@ -1,32 +1,74 @@
 package it.brasilenacode.chesshub.controller;
 
 
+import it.brasilenacode.chesshub.persistenza.DAO.UtenteDao;
 import it.brasilenacode.chesshub.persistenza.DBManager;
 import it.brasilenacode.chesshub.persistenza.model.Torneo;
 import it.brasilenacode.chesshub.persistenza.model.Utente;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.List;
 
 @RestController
 @CrossOrigin(value = "http://localhost:4200/")
 public class UtentiController {
-    @GetMapping("/getUtenti")
-    public List<Utente> getUtenti(HttpServletRequest req){
-        Utente utente = new Utente();
-        utente.setCognome("Rossi");
-        utente.setNome("Mario");
-        utente.setUsername("mario.rossi");
-        utente.setAdmin(false);
-        utente.setDataNascita(new java.util.Date());
-        utente.setPassword("password");
-        utente.setNazionalita("Italia");
-        utente.setPunteggio(132);
-        List<Utente> utenti = DBManager.getInstance().getUtenteDao().findAll();
-        utenti.add(utente);
-        //List<Utente> utenti= DBManager.getInstance().getUtenteDao().findAll();
-        return utenti;
+    @PostMapping("/utenti/getUtenti")
+    public List<Utente> getUtenti(HttpServletRequest req, @RequestBody String body){
+        String[] splitted = body.split("\\.");
+        String property = splitted[0];
+        String value = splitted[1];
+        UtenteDao ud = DBManager.getInstance().getUtenteDao();
+        return switch(property){
+            case "nome":{
+                yield ud.findAll().stream().filter(utente -> utente.getNome().equals(value)).toList();
+            }
+            case "cognome":{
+                yield ud.findAll().stream().filter(utente -> utente.getCognome().equals(value)).toList();
+            }
+            case "username":{
+                yield ud.findAll().stream().filter(utente -> utente.getUsername().equals(value)).toList();
+            }
+            case "nazionalita":{
+                yield ud.findAll().stream().filter(utente -> utente.getNazionalita().equals(value)).toList();
+            }
+            case "piuGiovaneDi":{
+                yield ud.findAll().stream().filter(utente ->
+                        utente.getDataNascita().toInstant().isAfter(LocalDate.parse(value,
+                                DateTimeFormatter.ofPattern("yyyy-MM-dd")).atStartOfDay().atZone(ZoneId.systemDefault())
+                                .toInstant()))
+                        .toList();
+            }
+            case "piuVecchioDi":{
+                yield ud.findAll().stream().filter(utente ->
+                        utente.getDataNascita().toInstant().isBefore(LocalDate.parse(value,
+                                DateTimeFormatter.ofPattern("yyyy-MM-dd")).atStartOfDay().atZone(ZoneId.systemDefault())
+                                .toInstant()))
+                        .toList();            }
+            case "natoIl":{
+                yield ud.findAll().stream().filter(utente ->
+                        utente.getDataNascita().toInstant().equals(LocalDate.parse(value,
+                                DateTimeFormatter.ofPattern("yyyy-MM-dd")).atStartOfDay().atZone(ZoneId.systemDefault())
+                                .toInstant()))
+                        .toList();
+            }
+            case "punteggioMaggiore":{
+                yield ud.findAll().stream().filter(utente -> utente.getPunteggio() > Integer.parseInt(value)).toList();
+            }
+            case "punteggioMinore":{
+                yield ud.findAll().stream().filter(utente -> utente.getPunteggio() < Integer.parseInt(value)).toList();
+            }
+            case "punteggioPreciso":{
+                yield ud.findAll().stream().filter(utente -> utente.getPunteggio() == Integer.parseInt(value)).toList();
+            }
+            default:{
+                yield ud.findAll();
+            }
+        };
     }
 }
 
