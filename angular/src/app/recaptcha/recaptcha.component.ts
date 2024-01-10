6423bba081e3e0466edd8fa321b5847d8493342c
Emanuele Galardo <galardo.emanuele@gmail.com>
2024-01-10 23:39:43 +0100

    Preparazione recaptcha

---
+++ angular/src/app/recaptcha/recaptcha.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recaptcha',
+  templateUrl: './recaptcha.component.html',
+  styleUrl: './recaptcha.component.css'
+})
+export class RecaptchaComponent {
+
+}
