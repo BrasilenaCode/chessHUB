8658fc19a08740ba02c872435198fd55c5a36a54
SalvatoreBia <salvatorebia02@gmail.com>
2024-01-15 21:46:33 +0100

    aggiustata a maps

--- angular/src/app/maps/maps.component.ts
+++ angular/src/app/maps/maps.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Torneo } from '../model/torneo';
+import { TorneoService } from '../services/torneo.service';
+import { UtentiService } from '../services/utenti.service';
+import { Utente } from '../model/utente';
 
 
 @Component({
@@ -12,46 +15,155 @@ import { Torneo } from '../model/torneo';
 export class MapsComponent implements OnChanges {
 
   @Input() torneo?: Torneo;
+  @Input() showEntirely: boolean = false;  
 
-  constructor(private http: HttpClient) {}
+
+  constructor(private http: HttpClient,
+     private torneoService: TorneoService) {}
+
   ngOnChanges(changes: SimpleChanges): void {
     this.searchLocation()
   }
 
+  displayDest: any;   // per gestire il marker del torneo vicino che si vuole visualizzare
+  display: any;       // per gestire il marker della propria posizione attuale
+  defaultCenter: google.maps.LatLngLiteral = {lat: 41.8719, lng: 12.5674};
+  center: google.maps.LatLngLiteral = {lat: 41.8719, lng: 12.5674};
+  
+  zoom = 6;
+  options = {}
+  location1: string = '';   // stringa che identifica la posizione attuale
+  distance: number = 0; 
+  showError: boolean = false;
+  errorMessage: string = '';
+  displayDefault: boolean = true; // se vera, viene mostrata la posizione di default sulla mappa (defaultCenter)
+  /*
+    se showEntirely è vera mostra solo la mappa con il luogo del torneo pinnato,
+    se falsa mostra tutta quanta la view dei tornei vicini 
+  */
+  tornei = new Map();           // coppia <chiave torneo | torneo>
+  listOfLocations = new Map();  // coppia <chiave torneo | boolean (per mostrarlo o meno)>
+  listToShow: { key: string, value: boolean }[] = [];   // array perché non si può iterare sulle mappe nell'html :(
 
-  display: any;
-  center: google.maps.LatLngLiteral = {lat: 24, lng: 12};
-  zoom = 10;
+  utente?: Utente;
 
-  moveMap(event: google.maps.MapMouseEvent) {
-    if (event.latLng != null) this.center = (event.latLng.toJSON());
+
+ngOnInit(): void {
+  try{
+    this.options = {
+      icon: '../../assets/foto/map-pin-solid.svg',
+      scaledSize: new google.maps.Size(40, 40),
+    };
+  } catch(e){}
+  // prendo tutti i tornei solo se devo cercare quelli vicini
+  if (this.showEntirely) {
+    this.torneoService.dammiTornei().subscribe(tornei => {
+      tornei.forEach(torneo => {
+        if (torneo.stato === 'prossimo') {
+          this.tornei.set(torneo.id, torneo);
+          this.listOfLocations.set(torneo.id, false);
+        }
+      })
+      this.listToShow = Array.from(this.listOfLocations, ([key, value]) => ({ key, value }))
+    });
   }
+}
 
-  move(event: google.maps.MapMouseEvent) {
-      if (event.latLng != null) this.display = event.latLng.toJSON();
-  }
 
-  getGeocoding(place: string) {
-    const apiKey = 'AIzaSyB8ar3GNTtk6U9ZGGhLNVDUAikh0On1AAo'
-    const apiUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(place)}&key=${apiKey}`;
-    return this.http.get(apiUrl)
-  }
+raiseError(messageToShow: string) {
+  this.showError = true;
+  this.errorMessage = messageToShow;
+  this.center = this.defaultCenter;
+  this.zoom = 5; 
+  this.displayDefault = true;
+}
 
-  markLocation(place: string) {
-    this.getGeocoding(place).subscribe((response: any) => {
-      let location: any;
-      if(response.results.length >0 && response.results[0].geometry != undefined) {
-        location = response.results[0].geometry.location;
-        this.display = {lat: location.lat, lng: location.lng}
+moveMap(event: google.maps.MapMouseEvent) {
+  if (event.latLng != null) this.center = (event.latLng.toJSON());
+}
+
+getGeocoding(place: string) {
+  const apiKey = 'AIzaSyB8ar3GNTtk6U9ZGGhLNVDUAikh0On1AAo'
+  const apiUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(place)}&key=${apiKey}`;
+  return this.http.get(apiUrl)
+}
+
+markLocation(place: string, iamhere: boolean) {
+  this.getGeocoding(place).subscribe((response: any) => {
+    let location: any;
+    if (response.results.length > 0 && response.results[0].geometry != undefined) {
+      location = response.results[0].geometry.location;
+      /*
+      iamhere serve per dire alla mappa di cambiare marker, se sono qui mostra l'svg
+      altrimenti il marker predefinito di google
+      */
+      if (iamhere) {
+        this.display = { lat: location.lat, lng: location.lng };
         this.center = this.display;
+      } else {
+        this.displayDest = { lat: location.lat, lng: location.lng };
+        this.center = this.displayDest;
       }
-    })
-  }
+      this.showError = false;
+      this.displayDefault = false;
+      this.zoom = 10;
+    } else {
+      this.raiseError("Nessun risultato.")
+    }
+  });
+}
 
-  searchLocation() {
+searchLocation() {
+  if (!this.showEntirely) {
     if (this.torneo != undefined) {
-      this.markLocation(this.torneo.luogo)
+      this.markLocation(this.torneo.luogo, false)
+    }
+  } else {
+    if (this.location1 != '') {
+      this.markLocation(this.location1, true)
+      this.calculateDistance()
     }
   }
+}
+
+
+
+calculateDistanceBetweenLocations(position1: google.maps.LatLngLiteral, position2: google.maps.LatLngLiteral): number {
+  const latLng1 = new google.maps.LatLng(position1.lat, position1.lng);
+  const latLng2 = new google.maps.LatLng(position2.lat, position2.lng);
+  return (google.maps.geometry.spherical.computeDistanceBetween(latLng1, latLng2))/1000;
+}
+
+calculateDistance() {
+  let atLeastOneTournament = false;
+  for (const [toCheck, torneo] of this.tornei.entries()) {
+    if (this.location1 && toCheck) {
+      this.getGeocoding(this.location1).subscribe((response1: any) => {
+        if (response1.results.length > 0 && response1.results[0].geometry != undefined) {
+          const location1 = response1.results[0].geometry.location;
+          this.getGeocoding(torneo.luogo).subscribe((response2: any) => {
+            if (response2.results.length > 0 && response2.results[0].geometry != undefined) {
+              const location2 = response2.results[0].geometry.location;
+              const calculatedDistance = parseFloat(
+                this.calculateDistanceBetweenLocations(
+                  { lat: location1.lat, lng: location1.lng },
+                  { lat: location2.lat, lng: location2.lng }
+                ).toFixed(2)
+              );
+              if (this.distance >= calculatedDistance) {
+                this.listOfLocations.set(toCheck, true);
+                atLeastOneTournament = true;
+              } else {
+                this.listOfLocations.set(toCheck, false);
+              }
+              this.listToShow = Array.from(this.listOfLocations, ([key, value]) => ({ key, value }));
+            }
+          });
+        }
+      });
+    }
+  }
+
+}
 
 }
