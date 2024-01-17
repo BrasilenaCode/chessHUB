3ac301bca8e331bd414f78371a7e2f890587e218
SalvatoreBia <salvatorebia02@gmail.com>
2024-01-17 12:58:46 +0100

    push del mail manager

---
+++ backend/src/main/java/it/brasilenacode/chesshub/mail/MailSettings.java
@@ -0,0 +1,11 @@
+package it.brasilenacode.chesshub.mail;
+
+public class MailSettings {
+
+    public static final String notifierSMTP = "smtp.gmail.com";
+
+    public static final String notifierMail = "chessHUB.brasilenaCode@gmail.com";
+
+    public static final String notifierPswd = ""; // inserire password della mail
+
+}
