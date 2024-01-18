2d76231fd6f9c4129d6a7579c83f31ea0852208b
Davide Pirrò <david.pirro02@gmail.com>
2024-01-18 16:36:24 +0100

    fix email

--- backend/src/main/java/it/brasilenacode/chesshub/mail/MailManager.java
+++ backend/src/main/java/it/brasilenacode/chesshub/mail/MailManager.java
@@ -1,8 +1,5 @@
 package it.brasilenacode.chesshub.mail;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import it.brasilenacode.chesshub.utilities.Pair;
 
 import java.util.*;
